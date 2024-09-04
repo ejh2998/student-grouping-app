6022ce0895329d045b8b07bb7284bdddc314ab27
Evan <ejh2998@gmail.com>
2024-09-04 13:18:45 -0400

    First Commit

---
+++ src/main/java/com/example/studentgroupingapp/StudentGroupingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentgroupingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGroupingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGroupingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentgroupingapp/StudentGroupingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentgroupingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGroupingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
